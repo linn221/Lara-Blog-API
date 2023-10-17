863b3db6caca594043324bb9e50e7edc3f462560
linn <linncoffeee@gmail.com>
2023-10-17 12:22:05 +0630

    done soft delete function

---
+++ app/Http/Controllers/PostDeleteController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostDeleteController extends Controller
+{
+    public function trash()
+    {
+        $trash_posts = Post::onlyTrashed()->get();
+        return PostResource::collection($trash_posts);
+    }
+
+    public function emptyTrash()
+    {
+        Post::onlyTrashed()->forceDelete();
+        return response()->noContent();
+    }
+
+    public function recycleTrash()
+    {
+        Post::onlyTrashed()->restore();
+        return response()->noContent();
+    }
+
+    public function recycleOne(string $id)
+    {
+        $post = Post::onlyTrashed()->where('id', $id)->first();
+        if (is_null($post)) {
+            return response()->json([
+                'message' => 'Post does not exist'
+            ], 404);
+        }
+        $post->restore();
+        return response()->noContent();
+
+    }
+
+    public function emptyOne(string $id)
+    {
+        $post = Post::onlyTrashed()->where('id', $id)->first();
+        if (is_null($post)) {
+            return response()->json([
+                'message' => 'Post does not exist'
+            ], 404);
+        }
+        $post->delete();
+        return response()->noContent();
+    }
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostDeleteController;
 use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,15 +24,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
+    Route::prefix('/trash/post')->controller(PostDeleteController::class)->group(function () {
+        Route::get('/', 'trash');
+        Route::post('/recycle-all', 'recycleTrash');
+        Route::post('/recycle/{id}', 'recycleOne');
+        Route::post('/empty-all', 'emptyTrash');
+        Route::post('/empty/{id}', 'emptyOne');
+    });
     Route::apiResource('/category', CategoryController::class);
     Route::apiResource('/post', PostController::class);
     Route::apiResource('/tag', TagController::class);
     Route::apiResource('/image', ImageController::class)->only(['index', 'store', 'destroy']);
 });
 
-Route::middleware('guest')->group(function() {
+Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
